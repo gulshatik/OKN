a7f46555ea09bde81aa569e1495bcdbfb5925654
gulshatik <91495478+gulshatik@users.noreply.github.com>
2021-11-15 02:06:47 +0500

    Add files via upload

---
+++ zadaniye3/zadaniye3/3.cpp
@@ -0,0 +1,24 @@
+//#include <iostream>
+//using namespace std;
+//
+//void prostiye_mnozh(int n)
+//{
+//	if (n < 2)
+//		return;
+//	int mnozh = 2;
+//	while (n % mnozh != 0) {
+//		mnozh++;
+//	}
+//	cout << mnozh << " ";
+//	if (n > 2)
+//		prostiye_mnozh(n / mnozh);
+//	return;
+//}
+//
+//int main()
+//{
+//	int N;
+//	cin >> N;
+//	prostiye_mnozh(N);
+//	return 0;
+//}
